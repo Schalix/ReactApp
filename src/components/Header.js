8debd666dce80772ea323619f7e90fb158afad50
schalksa <salearncode@gmail.com>
2021-01-18 10:17:58 +0100

    test button hover in header commented out

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 // import React from 'react'
-import React, { useState } from 'react';
+import React from 'react';
+// add this for hover (test)button after React: ", { useState }""
 // Only needed this for testing bootstrap. Can be deleted when test is over:
 // import Image from 'react-bootstrap/Image'
 
 // create header
 const Header = () => {
 
-    const [isShown, setIsShown] = useState(false);
+    // const [isShown, setIsShown] = useState(false);
 
     const styledDiv = {
         backgroundColor: 'azure',
@@ -19,7 +20,7 @@ const Header = () => {
         <header style={styledDiv}>
           <h1>Rick and Morty</h1>
           <h2> Discover the Caracters</h2>
-          <button
+          {/* <button
                         onMouseEnter={() => setIsShown(true)}
                         onMouseLeave={() => setIsShown(false)}>
                         Hover over me!
@@ -28,7 +29,7 @@ const Header = () => {
                             <div>
                                         I'll appear when you hover over the button.
                             </div> 
-                    )}
+                    )} */}
           {/* Test to see if bootstrap "rounded image" works. It does not. ????
           WANNA TEST AGAIN?! Un-comment import image above!
           <Image src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" rounded /> */}
